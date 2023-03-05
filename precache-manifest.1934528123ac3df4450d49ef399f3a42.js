05e465dd85822fc9cd91dff713f267c6c0a24a40
Furjad-Ahmad <malik5891954@gmail.com>
2023-03-05 13:38:51 +0500

    Furjad

---
+++ precache-manifest.1934528123ac3df4450d49ef399f3a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "402ed4c90106106287e2",
+    "url": "/static/css/main.905a0c36.chunk.css"
+  },
+  {
+    "revision": "402ed4c90106106287e2",
+    "url": "/static/js/main.402ed4c9.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "2f184a29335423ed3078",
+    "url": "/static/js/2.2f184a29.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "0a98de3c646c3f9364fc90666363f796",
+    "url": "/index.html"
+  }
+];
